a60aeae21efa22d5724c75fa7fb72a061c8dedbf
Isaac Xavier Desouza <66382402+isaacdes@users.noreply.github.com>
2022-03-28 13:33:24 +0530

    module7 Asiignmenet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LikeCompoenentComponent } from './like-compoenent/like-compoenent.compo
 import { InputFormatDirective } from './input-format.directive';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     LikeCompoenentComponent,
     InputFormatDirective,
     ZippyComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    CourseFormComponent
   ],
   imports: [
     BrowserModule,
